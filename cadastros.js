0c3e147f74b1ef2ecb9479a3d949ae5a9ac23d37
César dos Santos Pinto <132622537+ElCesarSP@users.noreply.github.com>
2024-04-08 02:03:33 -0300

    Criando teste de veficação
    Criando no nosso codígo de verificação de CPF para autenticar que o usuário esta preenchendo o seu dados pessoais para o nosso formulário de cadastro utilizando funções para realizar autentificação

--- cadastros.js
+++ cadastros.js
@@ -46,9 +46,68 @@ class cadastros {
                 if (!cliente.telefone) {
                     camposFaltando.push('Está faltando preenche o campo Telefone!')
                 }
-                if (!cliente.cpf) {
-                    camposFaltando.push('Está faltando preenche o campo CPF!')
+
+                function validarCPF(cpf) {
+                    cpf = cpf.replace(/[^\d]/g, ''); // Remove caracteres não numéricos
+
+                    if (
+                        cpf.length !== 11 ||
+                        cpf === "00000000000" ||
+                        cpf === "11111111111" ||
+                        cpf === "22222222222" ||
+                        cpf === "33333333333" ||
+                        cpf === "44444444444" ||
+                        cpf === "55555555555" ||
+                        cpf === "66666666666" ||
+                        cpf === "77777777777" ||
+                        cpf === "88888888888" ||
+                        cpf === "99999999999"
+                    ) {
+                        return false;
+                    }
+
+                    let soma = 0;
+                    for (let i = 0; i < 9; i++) {
+                        soma += parseInt(cpf.charAt(i)) * (10 - i);
+                    }
+
+                    let resto = 11 - (soma % 11);
+                    if (resto === 10 || resto === 11) {
+                        resto = 0;
+                    }
+
+                    if (resto !== parseInt(cpf.charAt(9))) {
+                        return false;
+                    }
+
+                    soma = 0;
+                    for (let i = 0; i < 10; i++) {
+                        soma += parseInt(cpf.charAt(i)) * (11 - i);
+                    }
+
+                    resto = 11 - (soma % 11);
+                    if (resto === 10 || resto === 11) {
+                        resto = 0;
+                    }
+
+                    if (resto !== parseInt(cpf.charAt(10))) {
+                        return false;
+                    }
+
+                    return true;
                 }
+
+                // Verificar se o CPF do cliente está preenchido e válido
+                if (!cliente.cpf) {
+                    camposFaltando.push('Está faltando preencher o campo CPF!');
+                } else {
+                    if (!validarCPF(cliente.cpf)) {
+                        camposFaltando.push('CPF inválido, por favor verificar!');
+                    }
+                }
+
+                //lembra de cria veficador de e-mail e cnh os que falta para fazer teste para nosso codigo!
+
                 if (!cliente.email) {
                     camposFaltando.push('Está faltando preenche o campo E-mail!')
                 }
