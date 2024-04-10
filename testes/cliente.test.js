c1bce86068b4c441dbf8fff94cb6ba034fc73fed
josellyto <josellyto_@hotmail.com>
2024-04-10 20:34:06 -0300

    add test para cliente.js

---
+++ testes/cliente.test.js
@@ -0,0 +1,34 @@
+const Cliente = require('../cliente');
+
+const novoCliente = new Cliente(
+    "Cesar Berberino",
+    "2000-01-24",
+    "não sabe ainda",
+    31,
+    "Rua alvorada, 666",
+    "(77) 9999-1020",
+    "123.456.789-00",
+    "unifg@gmail.com",
+    "1234567890",
+    "2010-01-01",
+    "2030-01-01",
+    "americano",
+    "Solteiro",
+    ""
+);
+
+console.log("Dados do Cliente:");
+console.log("Nome:", novoCliente.nome);
+console.log("Data de Nascimento:", novoCliente.dataNascimento);
+console.log("Gênero:", novoCliente.genero);
+console.log("Idade:", novoCliente.idade);
+console.log("Endereço:", novoCliente.endereco);
+console.log("Telefone:", novoCliente.telefone);
+console.log("CPF:", novoCliente.cpf);
+console.log("Email:", novoCliente.email);
+console.log("Carteira de Motorista:", novoCliente.carteiraMotorista);
+console.log("Emissão da CNH:", novoCliente.emissaoCNH);
+console.log("Validade da CNH:", novoCliente.validadeCNH);
+console.log("Nacionalidade:", novoCliente.nacionalidade);
+console.log("Estado Civil:", novoCliente.estadoCivil);
+console.log("Nome do Cônjuge:", novoCliente.nomeConjude);
