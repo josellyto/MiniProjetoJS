2a511d88ea9210f9bec00270eadd140afbb4bdf4
César dos Santos Pinto <132622537+ElCesarSP@users.noreply.github.com>
2024-06-18 18:06:48 -0300

    CRUD (Criar, Ler, Atualizar, Deletar) para veículos e vendas.
    CRUD de Veículos:
    
    Criar Veículo: Adicionar um novo veículo ao banco de dados.
    Ler Veículos: Buscar todos os veículos ou um veículo específico por ID.
    Atualizar Veículo: Atualizar as informações de um veículo existente.
    Deletar Veículo: Remover um veículo do banco de dados.
    CRUD de Vendas:
    
    Criar Venda: Registrar uma nova venda de veículo.
    Ler Vendas: Buscar todas as vendas ou uma venda específica por ID.
    Deletar Venda: Remover uma venda e atualizar o status do veículo relacionado.
    
    rotas para venda Controller

---
+++ VendaVeiculo/src/routes/vendaRoutes.js
@@ -0,0 +1,26 @@
+import { Router } from "express";
+import { createTabelaVeiculo, buscarVeiculos, buscarVeiculoID, insertVeiculo, updateVeiculo, deleteVeiculo } from "../controllers/veiculoController.js";
+
+const routerVeiculo = Router();
+
+// Rota principal para verificar se a API de veículos está funcionando
+routerVeiculo.get('/vehicle', (req, res) => {
+    res.json({
+        statusCode: 200,
+        message: "API Veículos funcionando!"
+    });
+});
+
+
+// Rota para buscar todos os veiculo
+routerVeiculo.get('/BuscarVeiculos', buscarVeiculos);
+// Rota para buscar um veiculo por ID
+routerVeiculo.get('/BuscarVeiculo', buscarVeiculoID);
+// Rota para inserir um novo veiculo
+routerVeiculo.post('/InserirVeiculo', insertVeiculo);
+// Rota para atualizar um veiculo
+routerVeiculo.put('/AtualizarVeiculo', updateVeiculo);
+// Rota para deletar um veiculo
+routerVeiculo.delete('/DeletarVeiculo', deleteVeiculo);
+
+export default routerVeiculo;
